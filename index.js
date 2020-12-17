98d77dd9f0649abec13227034981ac914f0783d6
BarBot <74660569+BarBot-spec@users.noreply.github.com>
2020-12-17 19:50:24 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2025,7 +2025,7 @@ if (text.includes('.ttp')){
             conn.sendMessage(id, buf, MessageType.image)
         })
     })
-else if (text == 'baka'){
+else if (text == 'Akbar'){
 let hasil = fs.readFileSync('mp3/' + 'baka' + '.wav')
  conn.sendMessage(id, hasil, MessageType.audio, { quoted: m } )
 }
